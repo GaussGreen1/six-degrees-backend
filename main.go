e064b56d246e632d8a038d0fc14767f0dee7613c
GaussGreen1 <favero.daniele123@yahoo.it>
2022-03-11 21:47:00 +0100

    api in post, links in table

--- main.go
+++ main.go
@@ -1,60 +1,114 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-type Article struct {
-	Id      string `json:"Id"`
-	Title   string `json:"title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
+type Games struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	Game     string             `bson:"game,omitempty"`
+	CharList []string           `bson:"charList,omitempty"`
 }
 
-// let's declare a global Articles array
-// that we can then populate in our main function
-// to simulate a database
-var Articles []Article
+type DegreesObject struct {
+	ID      primitive.ObjectID `bson:"_id,omitempty"`
+	Name    string             `bson:"name,omitempty"`
+	Degrees int                `bson:"degrees,omitempty"`
+	Links   []LinkObject       `bson:"links,omitempty"`
+}
+
+type LinkObject struct {
+	Character string `json:"character"`
+	Game      string `json:"game"`
+	Year      string `json:"year"`
+}
+
+type CharacterInput struct {
+	Name string `json:"name"`
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
-func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["id"]
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
-	// Loop over all of our Articles
-	// if the article.Id equals the key we pass in
-	// return the article encoded as JSON
-	for _, article := range Articles {
-		if article.Id == key {
-			json.NewEncoder(w).Encode(article)
-		}
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var input CharacterInput
+
+	println("reqBody1 = " + string(reqBody))
+	json.Unmarshal(reqBody, &input)
+	println("input = " + string(input.Name))
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://ryu:dBwlVrY7OVU93Y91@cluster0.he74l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
 	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
+
+	quickstartDatabase := client.Database("six-degrees")
+	degreesCollection := quickstartDatabase.Collection("degrees")
+
+	regex := bson.M{"$regex": primitive.Regex{Pattern: "^" + input.Name + "$", Options: "i"}}
+
+	filterCursor, err := degreesCollection.Find(ctx, bson.M{"name": regex})
+	if err != nil {
+		log.Fatal(err)
+	}
+	client.Disconnect(ctx)
+
+	var results []DegreesObject
+	if err = filterCursor.All(ctx, &results); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(results)
+
+	for _, element := range results {
+		fmt.Println(element.Degrees)
+		json.NewEncoder(w).Encode(element)
+	}
+
 }
 
 func handleRequests() {
-	// creates a new instance of a mux router
 	myRouter := mux.NewRouter().StrictSlash(true)
-	// replace http.HandleFunc with myRouter.HandleFunc
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/all", returnAllArticles)
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
 
-	//allow cors:
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+
 	myRouter.Headers("Access-Control-Allow-Origin", "*")
-	// finally, instead of passing in nil, we want
-	// to pass in our newly created router as the second
-	// argument
 
 	port := os.Getenv("PORT")
 	if len(port) <= 0 {
@@ -71,17 +125,6 @@ func handleRequests() {
 	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnAllArticles")
-	json.NewEncoder(w).Encode(Articles)
-}
-
 func main() {
-	Articles = []Article{
-		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-	}
 	handleRequests()
 }
-
-//https://tutorialedge.net/golang/creating-restful-api-with-golang/#getting-started-with-routers
